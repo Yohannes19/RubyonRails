05781678f4722e6f6af1039b6fffc0ee70d563dd
Yohannes Abrha Mulaw <joeabrha@gmail.com>
2024-06-04 23:13:40 +0200

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
     def index
         @portfolios_items = Portfolio.all
     end    
+    def new
+        @portfolios_item=Portfolio.new     
+    end    
+    
+    def create
+        @portfolios_item=Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body)) 
+    
+        respond_to do |format|
+          if @portfolios_item.save
+            format.html { redirect_to portfolios_path, notice: "Successfully is created and pubslished." }
+           
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            
+          end
+        end
+      end    
 end
